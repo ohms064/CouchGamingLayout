5ef2dda07f460222e0e698f0502662274d681368
Omar Rodríguez Pérez <omar.rodpe@gmail.com>
2021-08-15 19:34:49 -0500

    Removed Facebook SDK.

--- Assets/Project/Scripts/FacebookWeb/Login/FBLoginManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Doozy.Engine;
-using UnityEngine;
-
-public class FBLoginManager : MonoBehaviour {
-    public void OnSuccessLogin () {
-        Debug.Log ("Received succes login event");
-        GameEventMessage.SendEvent ("OnSuccessLogin");
-    }
-}
