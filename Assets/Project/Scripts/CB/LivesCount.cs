cc0ee3b61c7b650a2a9f5a4acba4d5af51ea99de
Omar Rodríguez Pérez <omar.rodpe@gmail.com>
2021-08-15 19:34:38 -0500

    Updated unity version 2021.1.4.

---
+++ Assets/Project/Scripts/CB/LivesCount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class LivesCount : MonoBehaviour {
+    [SerializeField]
+    private int _lives;
+    [SerializeField]
+    private TextMeshProUGUI _tmp;
+    [SerializeField]
+    private string _format = "{0}";
+
+    public void ReduceLives () {
+        _lives--;
+        _tmp.text = string.Format( _format, _lives );
+    }
+
+    public void AddeLives () {
+        _lives++;
+        _tmp.text = string.Format( _format, _lives );
+    }
+}
